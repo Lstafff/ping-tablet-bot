a3d30f5077b4e6f6e36ef4b1c0793fb399ce47f2
Gleb Plaxienko <72926378+Lstafff@users.noreply.github.com>
2026-07-04 02:59:17 +0300

    Add minimal web app scaffold

---
+++ web/src/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./styles.css";
+
+function App() {
+  return <main>Test</main>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
